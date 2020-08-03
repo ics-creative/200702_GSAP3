98daf8cfe99f4e6ab81ec837b9174ad548937ac7
ics-konno <konno@ics-web.jp>
2020-08-03 17:37:25 +0900

    DOMCcontentloadedを削除しscriptをdeferで読み込むよう変更

--- src/loading.ts
+++ src/loading.ts
@@ -1,29 +1,26 @@
 import { gsap } from "gsap";
 
-window.addEventListener("DOMContentLoaded", () => {
-  console.log("loading...");
-  // timelineを作成
-  const tl = gsap.timeline({
-    repeat: -1, // アニメーションの繰り返し回数。-1で無限回
-    repeatDelay: 0.3, // ループとループの間の時間
-    defaults: { duration: 0.5, ease: "power4.out" }, // tweenのデフォルトの値
-  });
-
-  // アニメーションを実行
-  tl.from(".box", {
-    scale: 0,
-  })
-    .to(".box1", {
-      left: 50,
-    })
-    .to(
-      ".box2",
-      {
-        right: 50,
-      },
-      "<"
-    ) // "<"は「前のアニメーションと同時に再生する」オプション
-    .to(".box", {
-      scale: 0,
-    });
+// timelineを作成
+const tl = gsap.timeline({
+  repeat: -1, // アニメーションの繰り返し回数。-1で無限回
+  repeatDelay: 0.3, // ループとループの間の時間
+  defaults: { duration: 0.5, ease: "power4.out" }, // tweenのデフォルトの値
 });
+
+// アニメーションを実行
+tl.from(".box", {
+  scale: 0,
+})
+  .to(".box1", {
+    left: 50,
+  })
+  .to(
+    ".box2",
+    {
+      right: 50,
+    },
+    "<"
+  ) // "<"は「前のアニメーションと同時に再生する」オプション
+  .to(".box", {
+    scale: 0,
+  });
--- src/main.ts
+++ src/main.ts
@@ -4,75 +4,72 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 // プラグインはgsap.registerPluginで登録
 gsap.registerPlugin(ScrollTrigger);
 
-window.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector("header");
+const button = document.querySelector("header");
 
-  const tween = gsap.to(button, {
-    duration: 0.5,
-    paused: true,
-    ease: "power2.out",
-    width: "100%",
-    height: "100px",
-    lineHeight: "100px",
-    borderRadius: "0%",
-    cursor: "default",
-    top: 0,
-    backgroundColor: "#0FBD94",
+const tween = gsap.to(button, {
+  duration: 0.5,
+  paused: true,
+  ease: "power2.out",
+  width: "100%",
+  height: "100px",
+  lineHeight: "100px",
+  borderRadius: "0%",
+  cursor: "default",
+  top: 0,
+  backgroundColor: "#0FBD94",
+});
+
+const showContent = () => {
+  // 以下のtween.play()とgsap.to()は同じことをしている
+  tween.play();
+  gsap.to("header h1", {
+    opacity: 1,
   });
-
-  const showContent = () => {
-    // 以下のtween.play()とgsap.to()は同じことをしている
-    tween.play();
-    gsap.to("header h1", {
-      opacity: 1,
-    });
-    // 画像郡を連続的に表示するアニメーションの制御
-    gsap.to(".img-container img", {
-      opacity: 1,
-      delay: 1,
-      duration: 1.5,
-      y: -10,
-      ease: "power2.out",
-      // 複数要素を扱うプロパティ
-      stagger: {
-        from: "start",
-        amount: 0.8,
+  // 画像郡を連続的に表示するアニメーションの制御
+  gsap.to(".img-container img", {
+    opacity: 1,
+    delay: 1,
+    duration: 1.5,
+    y: -10,
+    ease: "power2.out",
+    // 複数要素を扱うプロパティ
+    stagger: {
+      from: "start",
+      amount: 0.8,
+    },
+  });
+  // スクロールイベントの制御
+  gsap
+    .timeline({
+      defaults: { ease: "power2.out", duration: 1.5 },
+      scrollTrigger: {
+        markers: true, // マーカーを表示するか（開発用）
+        trigger: ".content", // この要素と交差するとイベントが発火
+        start: "top 50%", // ウィンドウのどの位置を発火の基準点にするか
+        end: "bottom 25%", // ウィンドウのどの位置をイベントの終了点にするか
+        toggleActions: "play none none none", // スクロールイベントで発火するアニメーションの種類
       },
-    });
-    // スクロールイベントの制御
-    gsap
-      .timeline({
-        defaults: { ease: "power2.out", duration: 1.5 },
-        scrollTrigger: {
-          markers: true, // マーカーを表示するか（開発用）
-          trigger: ".content", // この要素と交差するとイベントが発火
-          start: "top 50%", // ウィンドウのどの位置を発火の基準点にするか
-          end: "bottom 25%", // ウィンドウのどの位置をイベントの終了点にするか
-          toggleActions: "play none none none", // スクロールイベントで発火するアニメーションの種類
-        },
-      })
-      .to(".content-text h2", {
+    })
+    .to(".content-text h2", {
+      opacity: 1,
+      y: -10,
+    })
+    .to(
+      ".content-text p",
+      {
         opacity: 1,
         y: -10,
-      })
-      .to(
-        ".content-text p",
-        {
-          opacity: 1,
-          y: -10,
-        },
-        "-=1"
-      ) // 直前のアニメーションに0.7秒かぶせる
-      .to(
-        ".content img",
-        {
-          opacity: 1,
-          x: -10,
-        },
-        "-=1"
-      ); // 直前のアニメーションに0.7秒かぶせる
-  };
+      },
+      "-=1"
+    ) // 直前のアニメーションに0.7秒かぶせる
+    .to(
+      ".content img",
+      {
+        opacity: 1,
+        x: -10,
+      },
+      "-=1"
+    ); // 直前のアニメーションに0.7秒かぶせる
+};
 
-  if (!button) return;
-  button.addEventListener("click", showContent);
-});
+button.addEventListener("click", showContent);
